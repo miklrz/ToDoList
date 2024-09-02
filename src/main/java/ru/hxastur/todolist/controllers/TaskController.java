66a4e906750ed737fef7bea8bf5dd249f060f457
hxastur <arz.mikhail@gmail.com>
2024-09-02 17:43:28 +0300

    Doing TaskController and TaskService

--- src/main/java/ru/hxastur/todolist/controllers/TaskController.java
+++ src/main/java/ru/hxastur/todolist/controllers/TaskController.java
@@ -5,6 +5,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.hxastur.todolist.exceptions.AuthorNotFoundException;
@@ -13,22 +15,22 @@ import ru.hxastur.todolist.models.Author;
 import ru.hxastur.todolist.models.Task;
 import ru.hxastur.todolist.repositories.AuthorRepository;
 import ru.hxastur.todolist.repositories.TaskRepository;
+import ru.hxastur.todolist.service.TaskService;
 
-@RestController
+@Controller
 public class TaskController {
+    private TaskService taskService;
 
-    private final TaskRepository taskRepository;
-    private final AuthorRepository authorRepository;
-
-    public TaskController(TaskRepository taskRepository, AuthorRepository authorRepository){
-        this.taskRepository = taskRepository;
-        this.authorRepository = authorRepository;
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
     }
 
-    @GetMapping("/authors/{authorId}/tasks")
-    public Iterable<Task> getAllTasks(@PathVariable int authorId){
-        Author author =  authorRepository.findById(authorId).orElseThrow(()->new AuthorNotFoundException(authorId));
-        return author.getTaskList();
+    @GetMapping("/tasks")
+    public String getAuthorTasks(@PathVariable int authorId, Model model){
+//        Author author =  authorRepository.findById(authorId).orElseThrow(()->new AuthorNotFoundException(authorId));
+//        return author.getTaskList();
+        model.addAttribute("tasks", taskService.getAuthorTasks(id))
+        return "tasks";
     }
 
     @GetMapping("/authors/{authorId}/tasks/{id}")
