b014b344fb3f6044730bfa275d0578e3cefc6ede
hxastur <arz.mikhail@gmail.com>
2024-09-06 10:53:23 +0300

    edit and new html

--- src/main/java/ru/hxastur/todolist/controllers/UserController.java
+++ src/main/java/ru/hxastur/todolist/controllers/UserController.java
@@ -1,26 +1,18 @@
 package ru.hxastur.todolist.controllers;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 //import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.hxastur.todolist.exceptions.AuthorNotFoundException;
-import ru.hxastur.todolist.exceptions.TaskNotFoundException;
-import ru.hxastur.todolist.models.Author;
 import ru.hxastur.todolist.models.Task;
-import ru.hxastur.todolist.repositories.AuthorRepository;
-import ru.hxastur.todolist.repositories.TaskRepository;
 import ru.hxastur.todolist.service.TaskService;
 
 @Controller
 //@RequestMapping("/user")
 public class UserController {
+    // Вместо обращения по authorId в будущем нужно использовать Authorization
+    //
     private final TaskService taskService;
 
     public UserController(TaskService taskService){
@@ -31,19 +23,31 @@ public class UserController {
     public String getAuthorTasks(@PathVariable int authorId,Model model){
         model.addAttribute("taskList", taskService.getAuthorTasks(authorId));
         model.addAttribute("authorId", authorId);
-        return "tasks";
+        return "tasks/index";
     }
 
     @GetMapping("authors/{authorId}/tasks/{taskId}")
     public String getTask(@PathVariable int taskId, Model model, @PathVariable int authorId){
         model.addAttribute("task", taskService.getTask(taskId));
         model.addAttribute("authorId", authorId);
-        return "task";
+        return "tasks/task";
+    }
+
+    @GetMapping("authors/{authorId}/tasks/new")
+    public String newTaskPage(@PathVariable int authorId, Model model, @ModelAttribute("task") Task task){
+        model.addAttribute("authorId", authorId);
+        return "tasks/new";
+    }
+
+    @GetMapping("/authors/{authorId}/tasks/edit")
+    public String editTaskPage(@PathVariable int authorId, Model model, @ModelAttribute("task") Task task){
+        model.addAttribute("authorId", authorId);
+        return "tasks/edit";
     }
 
     @PostMapping("authors/{authorId}/tasks")
-    public String saveTask(@Valid @RequestBody Task newTask, @PathVariable int authorId){
-        taskService.saveTask(newTask,authorId);
+    public String saveTask(@RequestBody Task task, @PathVariable int authorId){
+        taskService.saveTask(task,authorId);
         return "redirect:/tasks";
     }
 
